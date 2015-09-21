1cca9b6d94f85607c51389d9ba11d8fd31a50145
Geoff Kimball <geoff@zurb.com>
2015-09-21 15:32:16 -0700

    Use Mocha for testing

---
+++ test/test.js
@@ -0,0 +1,17 @@
+var fr    = require('../index');
+var mocha = require('mocha');
+var vfs   = require('vinyl-fs');
+
+var SOURCES = './test/fixtures/*.html';
+var OUTPUT  = './test/fixtures/routes.js';
+
+describe('Front Router', function(done) {
+  it('generates a JavaScript object of routes from a set of HTML files', function(done) {
+    vfs.src(SOURCES)
+      .pipe(fr({
+        path: OUTPUT,
+        root: './test/fixtures'
+      }))
+      .on('finish', done);
+  });
+});
